c7f81ef678f65fdb8634bee015aff7f898084e04
dihan <dihan.algama@gmail.com>
2025-02-01 12:10:26 +0000

    Adding Ultra 1.2 workflow to ui

--- docker/setup.sh
+++ docker/setup.sh
@@ -11,8 +11,8 @@ echo "
 mkdir -p /workspace/ComfyUI/models/{diffusion_models,text_encoders,vae,upscale,loras}
 
 # Ensure workflow is in place
-if [ ! -f /workspace/ComfyUI/AllinOne1.4.json ]; then
-    cp /workspace/AllinOne1.4.json /workspace/ComfyUI/user/default/workflows/AllinOne1.4.json
+if [ ! -f /workspace/ComfyUI/AllinOneUltra1.2.json ]; then
+    cp /workspace/AllinOneUltra1.2.json /workspace/ComfyUI/user/default/workflows/AllinOneUltra1.2.json
 fi
 " >> /workspace/start.sh
 
