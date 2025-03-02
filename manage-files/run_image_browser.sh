f3eac397e1b46fbb1637bc8f3d8fd40c0e44d868
Dihan <dihan.algama@gmail.com>
2025-03-02 10:39:29 +0000

    fixed root path on start image browser

--- manage-files/run_image_browser.sh
+++ manage-files/run_image_browser.sh
@@ -2,4 +2,4 @@
 # File: /workspace/run_image_browser.sh
 
 cd /workspace/sd-webui-infinite-image-browsing
-python app.py --port=8181 --root="/workspace/ComfyUI/output" --host="0.0.0.0"
+python app.py --port=8181 --extra_paths /workspace/ComfyUI/output --host=0.0.0.0
