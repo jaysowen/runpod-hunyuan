c11b28a2d443f74ee41c62ec85549f0712978181
jaysowen <jaysowen@163.com>
2025-05-15 15:43:59 +0800

    为 hyper_lora 模型添加符号链接

--- scripts/pre_start.sh
+++ scripts/pre_start.sh
@@ -50,6 +50,7 @@ create_model_symlink "/runpod-volume/ComfyUI/models/landmarks" "/workspace/Comfy
 create_model_symlink "/runpod-volume/ComfyUI/models/sams" "/workspace/ComfyUI/models/sams"
 create_model_symlink "/runpod-volume/ComfyUI/models/segformer_b3_clothes" "/workspace/ComfyUI/models/segformer_b3_clothes"
 create_model_symlink "/runpod-volume/ComfyUI/models/grounding-dino" "/workspace/ComfyUI/models/grounding-dino"
+create_model_symlink "/runpod-volume/ComfyUI/models/hyper_lora" "/workspace/ComfyUI/models/hyper_lora"
 
 # --- End Symlink Creation ---
 
